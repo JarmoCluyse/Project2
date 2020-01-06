cf6f16711cba6380fb0498b46b142b61f6c3d9f0
DebieThomas <thomas.debie@student.howest.be>
2020-01-06 15:07:30 +0100

    add car

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ var config = {
     width: window.innerWidth * window.devicePixelRatio,
     height: window.innerHeight * window.devicePixelRatio,
     autoResize: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
@@ -19,11 +26,15 @@ var game = new Phaser.Game(config);
 function preload ()
 {
     this.load.image('road', 'assets/road.png');
+    this.load.image('car', 'assets/car.png');
 }
+var cars;
 
 function create ()
 {
-    this.background1 = this.add.tileSprite(window.innerWidth * window.devicePixelRatio /2,window.innerHeight * window.devicePixelRatio /2,480,700, 'road').setScale(2.5)
+    this.background1 = this.add.tileSprite(window.innerWidth * window.devicePixelRatio /2,window.innerHeight * window.devicePixelRatio /2,480,700, 'road').setScale(2.5);
+    cars = this.physics.add.staticGroup();
+    cars.create(400, 568, 'car').setScale(0.2).refreshBody();
 }
 
 function update ()
