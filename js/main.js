6cccf8216a019195729a590ca758d98984044c8f
DebieThomas <thomas.debie@student.howest.be>
2020-01-07 12:21:33 +0100

    make car movable

--- js/main.js
+++ js/main.js
@@ -28,14 +28,40 @@ function preload ()
 }
 var cars;
 
+
 function create ()
 {
     this.background1 = this.add.tileSprite(240,300,480,600, 'road')
     cars = this.physics.add.staticGroup();
     cars.create(60,550, 'car').setScale(1).refreshBody();
+    this.input.keyboard.on('keydown-RIGHT', moveCar);
+    this.input.keyboard.on('keydown-UP', moveCar);
+    this.input.keyboard.on('keydown-DOWN', moveCar);
+    this.input.keyboard.on('keydown-LEFT', moveCar);
 }
 
 function update ()
 {
     this.background1.tilePositionY -= 0.5
 }
+function moveCar(e)
+{
+    console.log('move');
+    console.log(e);
+    console.log(cars);
+    if (e.key == "ArrowLeft"){
+        cars.children.entries[0].x = 60;
+    }
+    if (e.key == "ArrowUp"){
+        cars.children.entries[0].x = 180;
+    }
+    if (e.key == "ArrowDown"){
+        cars.children.entries[0].x = 300;
+    }
+    if (e.key == "ArrowRight"){
+        cars.children.entries[0].x = 420;
+    }
+
+    
+}
+
