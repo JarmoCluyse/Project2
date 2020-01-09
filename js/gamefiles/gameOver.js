5a98cea7794f57d6f66006dc7a2ac1e9d522a207
Jarmo Cluyse <jarmo.cluyse@student.howest.be>
2020-01-09 15:17:39 +0100

    hotfix 2 game name uppercase

--- js/gamefiles/gameOver.js
+++ js/gamefiles/gameOver.js
@@ -1,53 +1,53 @@
 var mainMenuState = new Phaser.Class({
-    // Define scene
-    Extends: Phaser.Scene,
-    initialize:
-    function GameOver(){
-        Phaser.Scene.call(this, {key: 'GameOver'});
-    },
-  
-    preload: function() {
-        // Preload images for this state
-    },
+	// Define scene
+	Extends: Phaser.Scene,
+	initialize: function GameOver() {
+		Phaser.Scene.call(this, { key: 'GameOver' });
+	},
 
-    create: function() {
-        // log the current scene
-        console.log("scene: GameOver");
-        this.background1 = this.add.tileSprite(240,400,480,800, 'white')
-        // page
-        Text = this.add.text(20, 300, 'score: ', { fontSize: '32px', fill: '#747474' });
-        Text2 = this.add.text(20, 400, 'press an arrow ', { fontSize: '32px', fill: '#747474' });
-        Text.setText('Score: ' + score);
+	preload: function() {
+		// Preload images for this state
+	},
 
+	create: function() {
+		// log the current scene
+		console.log('scene: GameOver');
+		this.background1 = this.add.tileSprite(240, 400, 480, 800, 'white');
+		// page
+		Text = this.add.text(20, 300, 'score: ', {
+			fontSize: '32px',
+			fill: '#747474'
+		});
+		Text2 = this.add.text(20, 400, 'press an arrow ', {
+			fontSize: '32px',
+			fill: '#747474'
+		});
+		Text.setText('Score: ' + score);
 
+		// further
+		this.input.keyboard.on('keydown-RIGHT', listener2);
+		this.input.keyboard.on('keydown-UP', listener2);
+		this.input.keyboard.on('keydown-DOWN', listener2);
+		this.input.keyboard.on('keydown-LEFT', listener2);
+	},
 
-        // further
-        this.input.keyboard.on('keydown-RIGHT', listener2);
-        this.input.keyboard.on('keydown-UP', listener2);
-        this.input.keyboard.on('keydown-DOWN', listener2);
-        this.input.keyboard.on('keydown-LEFT', listener2);
-
-    },
-
-    update: function() {
-        // Update objects & variables
-    },
+	update: function() {
+		// Update objects & variables
+	}
 });
 
-function listener2 () {
-    // only listen if game is over
-    if (gameStarted && gameOver){
-        // stop this scene start the begin scene
-        game.scene.stop('GameOver');
-        game.scene.start('MainMenu');
-        // put the gamestarted and gameover on false
-        score = 0
-        gameStarted = false;
-        gameOver = false;
-        gameDone = false;
-    }
-    
-
+function listener2() {
+	// only listen if game is over
+	if (gameStarted && gameOver) {
+		// stop this scene start the begin scene
+		game.scene.stop('GameOver');
+		game.scene.start('MainMenu');
+		// put the gamestarted and gameover on false
+		score = 0;
+		gameStarted = false;
+		gameOver = false;
+		gameDone = false;
+	}
 }
 // Add scene to list of scenes
 myGame.scenes.push(mainMenuState);
--- js/gamefiles/main.js
+++ js/gamefiles/main.js
@@ -1,24 +1,24 @@
-        //On page load, create game config
-        var config = {
-            // Rendering auto
-            type: Phaser.AUTO,
-            // Width and height of the game
-            width: 480,
-            height: 800,
-            // Enable physics
-            physics: {
-                default: 'arcade',
-                arcade: {
-                    // Disable gravity
-                    // Physics needded for collisions
-                    gravity: { y: 0 },
-                    // shows hitboxes
-                    debug: false
-                }
-            },
-            // id for html
-            parent:'phaser',
-            scene: myGame.scenes
-        };
-        //Instantiate the game with the config
-        var game = new Phaser.Game(config);
+//On page load, create game config
+var config = {
+	// Rendering auto
+	type: Phaser.AUTO,
+	// Width and height of the game
+	width: 480,
+	height: 800,
+	// Enable physics
+	physics: {
+		default: 'arcade',
+		arcade: {
+			// Disable gravity
+			// Physics needded for collisions
+			gravity: { y: 0 },
+			// shows hitboxes
+			debug: false
+		}
+	},
+	// id for html
+	parent: 'phaser',
+	scene: myGame.scenes
+};
+//Instantiate the game with the config
+var game = new Phaser.Game(config);
