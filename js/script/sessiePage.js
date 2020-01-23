b9f1cfe04e8cc54c4153f2403c1fa9a4aeb2a5d4
Wout Ruyters <wout.ruyters@student.howest.be>
2020-01-23 15:27:04 +0100

    SessionPage almost finished

--- js/script/sessiePage.js
+++ js/script/sessiePage.js
@@ -23,6 +23,25 @@ const getSessionResults = function(){
 };
 
 
+const showNewSessionpage = function(){
+    newSessionPage.style.display = 'block';
+    sessionSelectSubjectDropdown.innerHTML = "";
+    sessionSelectSubjectDropdown.innerHTML += `<option value="${subjectIdHier}">${subjectNaamHier}</option>`
+
+    hideMainPage();
+
+};
+
+const showSessionMainPage = function(){
+    newSessionPage.style.display = 'none';
+    deleteSessionPage.style.display = 'none';
+    deleteSesssionConfirmation.style.display = 'none';
+    newSessionIdCard.style.display = 'none';
+    sessionMainPage.pointerEvents = 'auto';
+    sessionMainPage.style.opacity = '1';
+
+};
+
 const fillSessionDropdown = function(){
     sessionDropdown.innerHTML = "";
 
@@ -34,16 +53,92 @@ const fillSessionDropdown = function(){
 };
 
 
+const hideMainPage = function(){
+    sessionMainPage.pointerEvents = 'none';
+    sessionMainPage.style.opacity = '0.8';
+};
+
+const showDeleteSessionPage = function(){
+    deleteSessionPage.style.display = 'block';
+    sessionSelectDeleteDropdown.innerHTML = "";
+    sessionSelectDeleteDropdown.innerHTML += `<option value="${sessieIdHier}">${sessieNaamHier}</option>`
+    hideMainPage();
+};
+
+const showDeleteSessionConfirmation = function(){
+    deleteSesssionConfirmation.style.display = 'block';
+    deleteSessionPage.style.display = 'none';
+}
+
+const deleteSession = function(){
+    // verwijder hier je de session
+
+    showSessionMainPage();
+};
+
+const showSessionIdPage = function(){
+    newSessionPage.style.display = 'none';
+    newSessionIdCard.style.display = 'flex';
+    
+
+};
+
 const init = function(){
     console.log('Script geladen! 👍')
     sessionDropdown = document.querySelector('.js-sessionSelection');
     resultsTitle = document.querySelector('.c-session-card-results-title');
+    newSessionButton = document.querySelector('.js-newSession');
+    createNewSessionButton = document.querySelector('.js-addNewSession');
+    closeButton = document.querySelectorAll('.c-close__button');
+    newSessionPage = document.querySelector('.c-newSession-card');
+    sessionMainPage = document.querySelector('.c-main-card');
+    deleteSessionPage = document.querySelector('.c-deleteSession-card');
+    deleteSesssionConfirmation = document.querySelector('.c-delete__session');
+    deleteASessionButton = document.querySelector('.js-deleteASession');
+    deleteSessionButton = document.querySelector('.js-deleteSessionButton');
+    yesButton = document.querySelector('.js-yesSession');
+    noButton = document.querySelector('.js-noSession');
+    newSessionIdCard = document.querySelector('.c-sessionId-card');
+    sessionSelectSubjectDropdown = document.getElementById('selectSessionSubject');
+    sessionSelectDeleteDropdown = document.getElementById('deleteSessionSelect');
 
     fillSessionDropdown();
 
     resultsTitle.innerHTML = `Dit zijn de resultaten voor ${sessionDropdown.options[sessionDropdown.selectedIndex].innerHTML}:`
     dropdownId = sessionDropdown.options[sessionDropdown.selectedIndex].value
 
+    deleteASessionButton.addEventListener('click', function(){
+        showDeleteSessionPage();
+    });
+
+
+    closeButton.forEach(element => {
+        element.addEventListener('click', function(){
+            showSessionMainPage();
+        });    
+    });
+
+    deleteSessionButton.addEventListener('click', function(){
+        selectedSessionToDelete = sessionSelectDeleteDropdown.options[sessionSelectDeleteDropdown.selectedIndex].value;
+        showDeleteSessionConfirmation();
+    });
+
+    createNewSessionButton.addEventListener('click', function(){
+        selectedSessionSubject = sessionSelectSubjectDropdown.options[sessionSelectSubjectDropdown.selectedIndex].value;
+        showSessionIdPage();
+    });
+
+    newSessionButton.addEventListener('click', function(){
+        showNewSessionpage();
+    });
+
+    noButton.addEventListener('click', function(){
+        showSessionMainPage();
+    });
+
+    yesButton.addEventListener('click', function(){
+        deleteSession();
+    });
 
     sessionDropdown.addEventListener('change', function(){
         console.log(sessionDropdown.options[sessionDropdown.selectedIndex].value)  
@@ -51,7 +146,7 @@ const init = function(){
         resultsTitle.innerHTML = `Dit zijn de resultaten voor ${sessionDropdown.options[sessionDropdown.selectedIndex].innerHTML}:`
 
         getSessionResults();
-    })
+    });
 }
 
 
