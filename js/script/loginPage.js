5886f96372d5af65d6918ba6850cd84ac01c0ba3
Wout Ruyters <wout.ruyters@student.howest.be>
2020-01-24 12:06:18 +0100

    Revert "Merge remote-tracking branch 'origin/development' into development"
    This reverts commit 729c4b274ef67dd7a5fadb4058ac343c30220232.

--- js/script/loginPage.js
+++ js/script/loginPage.js
@@ -214,7 +214,7 @@ const checkCallback = function(data){//This function checks if the logintoken st
 
 const init = function(){
 	console.log('Script geladen! 👍')
-	token = JSON.parse(localStorage.getItem("LoginToken"));
+	let token = JSON.parse(localStorage.getItem("LoginToken"));
 	console.log(token);
 	if (token != null){
 		sendData(`${BASEURI}login/token?code=${key}`, checkCallback, "POST", token);
