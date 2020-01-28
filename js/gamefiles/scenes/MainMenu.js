376330c975cfd4ff7554a19c599e92b55ff483ce
Jarmo Cluyse <jarmo.cluyse@student.howest.be>
2020-01-28 16:28:50 +0100

    updated to ../

--- js/gamefiles/scenes/MainMenu.js
+++ js/gamefiles/scenes/MainMenu.js
@@ -3,50 +3,48 @@
 //------------------------------- //
 
 var mainMenuState = new Phaser.Class({
-    // Define scene
-    Extends: Phaser.Scene,
-    initialize:
-    function MainMenu(){
-        Phaser.Scene.call(this, {key: 'MainMenu'});
-    },
-    preload: function() {
-        // in seperate file Preloads
-    },
-    create: function() {
-        // log the current scene
-        console.log("MainMenu");
-        // -------------------------- //
-        // make front page
-        // -------------------------- //
-        localStorage.removeItem('SessionObject');
-        handleData(`${BASEURI}session/${session}?code=${key}`, setSession);
-        if(mode == 'COOP'){ // change video if coop 
-            jsVideo.innerHTML = `<source src="/assets/vidCoop.m4v" type="video/mp4">
+	// Define scene
+	Extends: Phaser.Scene,
+	initialize: function MainMenu() {
+		Phaser.Scene.call(this, { key: 'MainMenu' });
+	},
+	preload: function() {
+		// in seperate file Preloads
+	},
+	create: function() {
+		// log the current scene
+		console.log('MainMenu');
+		// -------------------------- //
+		// make front page
+		// -------------------------- //
+		localStorage.removeItem('SessionObject');
+		handleData(`${BASEURI}session/${session}?code=${key}`, setSession);
+		if (mode == 'COOP') {
+			// change video if coop
+			jsVideo.innerHTML = `<source src="../assets/vidCoop.m4v" type="video/mp4">
             Your browser does not support the video tag.`;
-        }
-        else{
-            jsVideo.innerHTML = `<source src="/assets/vid.m4v" type="video/mp4">
+		} else {
+			jsVideo.innerHTML = `<source src="../assets/vid.m4v" type="video/mp4">
             Your browser does not support the video tag.`;
-        }
-        // -------------------------- //
-        // Hide things
-        // -------------------------- //
-        jsMainCard.classList.add("c-start-card");
-        jsGameStart.classList.remove('hide');
-        jsGamePlay.classList.add('hide');
-        jsGameQuestion.classList.add('hide');;
-        // -------------------------- //
-        // listeners for next scene
-        // -------------------------- //
-        this.input.keyboard.on('keydown-RIGHT', keyListener);
-        this.input.keyboard.on('keydown-UP', keyListener);
-        this.input.keyboard.on('keydown-DOWN', keyListener);
-        this.input.keyboard.on('keydown-LEFT', keyListener);
-        
-    },
-    update: function() {
-        // Update objects & variables
-    },
+		}
+		// -------------------------- //
+		// Hide things
+		// -------------------------- //
+		jsMainCard.classList.add('c-start-card');
+		jsGameStart.classList.remove('hide');
+		jsGamePlay.classList.add('hide');
+		jsGameQuestion.classList.add('hide');
+		// -------------------------- //
+		// listeners for next scene
+		// -------------------------- //
+		this.input.keyboard.on('keydown-RIGHT', keyListener);
+		this.input.keyboard.on('keydown-UP', keyListener);
+		this.input.keyboard.on('keydown-DOWN', keyListener);
+		this.input.keyboard.on('keydown-LEFT', keyListener);
+	},
+	update: function() {
+		// Update objects & variables
+	}
 });
 // -------------------------- //
 // Add scene to list of scenes
@@ -55,7 +53,7 @@ myGame.scenes.push(mainMenuState);
 // -------------------------- //
 // Listen to session
 // -------------------------- //
-setSession = function(data){
-    console.log(data);
-    localStorage.setItem('SessionObject', JSON.stringify(data));
+setSession = function(data) {
+	console.log(data);
+	localStorage.setItem('SessionObject', JSON.stringify(data));
 };
